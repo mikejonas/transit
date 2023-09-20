68a31a22d7c859b0c5e798e714793e8c09d64cac
Mike Jonas <mikejonas@users.noreply.github.com>
2023-09-20 15:52:58 -0700

    Transit animation with mock data (#15)
    * animate transit
    
    * animation mvp

--- react-native-app/src/screens/TransitDetails/index.tsx
+++ react-native-app/src/screens/TransitDetails/index.tsx
@@ -1,6 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { FlatList, SafeAreaView } from 'react-native'
-import { InteractionManager } from 'react-native'
 import { RouteProp, useRoute } from '@react-navigation/native'
 import { useTheme } from '@shopify/restyle'
 import Box from 'components/Box'
@@ -9,16 +8,10 @@ import { ThumbsUp, ThumbsDown } from 'components/Icons'
 import Input from 'components/Input'
 import Text from 'components/Text'
 import { DrawerNavigatorParams } from 'navigators/MainNavigator'
-import { requests } from 'requests'
 import { Theme } from 'theme/restyle'
 import ChatMessage from './components/ChatMessage'
-import {
-  // MOCK_ASSISTANT_MESSAGE,
-  Message,
-  makeTemporaryBotResponse,
-  makeUserSubmittedMessage,
-  ASSISTANT_ROLE,
-} from './utils'
+import TransitGrid from './components/TransitGrid'
+import { Message } from './utils'
 
 /**
  * This is a rough hacked together version of chat ui. It would need to be cleaned up to build upon
@@ -31,108 +24,9 @@ const ChatScreen: React.FC = () => {
   const [hasDataInitialized, setHasDataInitialized] = useState(false)
   const [isInitialLoading, setIsInitialLoading] = useState(true)
   const flatListRef = useRef<FlatList>(null)
-  const route = useRoute<RouteProp<DrawerNavigatorParams, 'DailyHoroscope'>>()
+  const route = useRoute<RouteProp<DrawerNavigatorParams, 'TransitList'>>()
 
   const { conversationId } = route.params
-  const startConversation = async () => {
-    if (conversationId) {
-      const { data } = await requests.edgeFunctions.startConversation({ conversationId })
-      setMessages([data] as Message[])
-    } else {
-      console.error('conversationId is undefined')
-    }
-  }
-
-  const getMessages = async () => {
-    const { data } = await requests.generated.getMessages({ conversationId })
-    if (!data || data.length === 0) {
-      await startConversation()
-    } else {
-      setMessages(data.reverse() as Message[])
-    }
-    setHasDataInitialized(true)
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await getMessages()
-      setIsInitialLoading(false)
-    }
-
-    fetchData()
-  }, [])
-
-  useEffect(() => {
-    if (messages.length > 0) {
-      scrollToBottom()
-    }
-  }, [messages])
-
-  const postMessage = async (newMessage: string) => {
-    if (newMessage === '') return
-
-    const lastMessageId = messages[messages.length - 1].message_id
-    const conversationId = messages[0]!.conversation_id
-
-    if (lastMessageId === undefined || conversationId === undefined) {
-      console.error('lastMessageId or conversationId is undefined', {
-        lastMessageId,
-        conversationId,
-      })
-      return
-    }
-
-    setInputText('')
-    addUserMessage(makeUserSubmittedMessage(newMessage, lastMessageId + 1))
-    addTemporaryResponse()
-    try {
-      const { data } = await requests.edgeFunctions.addMessage({
-        newMessage: newMessage,
-        conversationId,
-      })
-      replaceTemporaryResponseWithBotResponse(data)
-    } catch (error: any) {
-      // do something with error (error.message)
-    }
-  }
-
-  const TEMP_MESSAGE_ID = 999999999
-
-  const addTemporaryResponse = () => {
-    const tempMessage = makeTemporaryBotResponse(TEMP_MESSAGE_ID)
-    setMessages(previousMessages => [...previousMessages, tempMessage])
-  }
-
-  const replaceTemporaryResponseWithBotResponse = (botMessage: Message) => {
-    setMessages(previousMessages => [
-      ...previousMessages.filter(message => message.message_id !== TEMP_MESSAGE_ID),
-      botMessage,
-    ])
-  }
-
-  const addUserMessage = (botMessage: Message) => {
-    setMessages(previousMessages => [...previousMessages, botMessage])
-  }
-
-  const scrollToBottom = () => {
-    const lastMessageIndex = messages.length - 1
-
-    InteractionManager.runAfterInteractions(() => {
-      setTimeout(() => {
-        flatListRef.current?.scrollToIndex({
-          animated: !hasDataInitialized,
-          index: lastMessageIndex,
-        })
-      }, 100)
-    })
-  }
-
-  const onScrollToIndexFailed = (info: { index: number }) => {
-    const wait = new Promise(resolve => setTimeout(resolve, 100))
-    wait.then(() => {
-      flatListRef.current?.scrollToIndex({ index: info.index, animated: !hasDataInitialized })
-    })
-  }
 
   const renderGrid = () => {
     return (
@@ -199,35 +93,29 @@ const ChatScreen: React.FC = () => {
     )
   }
 
-  return (
-    <Box flex={1} backgroundColor="background">
-      <Box m="l">{renderGrid()}</Box>
-      <FlatList
-        ref={flatListRef}
-        data={messages}
-        keyExtractor={item => item.message_id}
-        renderItem={({ item }) => (
-          <Box marginBottom="m">
-            <ChatMessage messageRole={item.role} messageContent={item.content} />
-          </Box>
-        )}
-        contentContainerStyle={{ padding: theme.spacing.m }}
-        onScrollToIndexFailed={onScrollToIndexFailed}
-      />
-      <SafeAreaView>
-        <Box flexDirection="row" alignItems="center" marginHorizontal="m" marginTop="s">
-          <Box flex={1}>
-            <Input
-              value={inputText}
-              onChangeText={setInputText}
-              placeholder="Type your message here"
-            />
-          </Box>
-          <Box marginLeft="s">
-            <Button icon="UP_ARROW" onPress={() => postMessage(inputText)} />
-          </Box>
+  const renderChat = () => (
+    <SafeAreaView>
+      <Box flexDirection="row" alignItems="center" marginHorizontal="m" marginTop="s">
+        <Box flex={1}>
+          <Input
+            value={inputText}
+            onChangeText={setInputText}
+            placeholder="Type your message here"
+          />
         </Box>
-      </SafeAreaView>
+        <Box marginLeft="s">
+          <Button icon="UP_ARROW" onPress={() => console.log(inputText)} />
+        </Box>
+      </Box>
+    </SafeAreaView>
+  )
+
+  return (
+    <Box mt="m">
+      <Box m="m">
+        <TransitGrid />
+      </Box>
+      {/* {renderChat()} */}
     </Box>
   )
 }
